46ced0e1d81f9c64c7a4439b61db55d9d26b48ed
Andrew Litvinenko <dandeliondevelop@gmail.com>
2023-02-22 15:49:23 +0700

    Removed old code

--- src/edu/uni/lab/IBehaviour.java
+++
@@ -1,5 +0,0 @@
-package edu.uni.lab;
-
-public interface IBehaviour {
-	public void move(int xСoord, int yCoord);
-}
--- src/edu/uni/lab/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.uni.lab;
-
-import edu.uni.lab.Habitat;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
-}
