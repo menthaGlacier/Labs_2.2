b96de971289d16de60df7f47e6a067144db081c4
Vortigon <vort.gonov.2016@gmail.com>
2023-06-17 13:13:35 +0700

    fat jar maven

---
+++ src/main/java/edu/uni/lab/ClientSideMain.java
@@ -0,0 +1,7 @@
+package edu.uni.lab;
+
+public class ClientSideMain {
+	public static void main(String[] args) {
+		Main.main(args);
+	}
+}
---
+++ src/main/java/edu/uni/lab/ServerSideMain.java
@@ -0,0 +1,9 @@
+package edu.uni.lab;
+
+import edu.uni.lab.server.Server;
+
+public class ServerSideMain {
+	public static void main(String[] args) {
+		Server.main(args);
+	}
+}
